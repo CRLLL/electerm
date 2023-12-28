5ebf12c9aa9957b682af1cac068acf18c033eef1
ZHAO Xudong <zxdong@gmail.com>
2023-12-28 11:24:23 +0800

    fix #3207 #3205 #3208 #3206 Fix backspace key issue, fix unexpected drag
    (cherry picked from commit e8ff95d476982b756e8d4112d022cbb9da3e7657)

--- src/client/common/create-title.jsx
+++ src/client/common/create-title.jsx
@@ -34,6 +34,9 @@ export default function createTitle (res) {
 
 export function createTitleTag (obj) {
   const { color } = obj
+  if (!color) {
+    return null
+  }
   const styleTag = color
     ? { color }
     : {}
